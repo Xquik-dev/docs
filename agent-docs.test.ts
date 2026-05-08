47d8284fd971d3d02059a5c7ffbeff048d983964
kriptoburak <kriptoburak@users.noreply.github.com>
2026-05-08 04:31:31 +0300

    Increase agent docs CI timeout

--- agent-docs.test.ts
+++ agent-docs.test.ts
@@ -1,3 +1,5 @@
 import { describeAgentDocsPerCheck } from 'afdocs/helpers';
 
-describeAgentDocsPerCheck(undefined, 180_000);
+const LIVE_AGENT_DOCS_TIMEOUT_MS = 300_000;
+
+describeAgentDocsPerCheck(undefined, LIVE_AGENT_DOCS_TIMEOUT_MS);
